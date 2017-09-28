3bc76faeba73889ed0c761a08717a23a4abcb38a
Sayujya Rijal <srijal@uwaterloo.ca>
2017-09-27 23:40:56 -0400

    added service worker, try 5

--- sw.js
+++ sw.js
@@ -6,7 +6,19 @@ self.addEventListener('install', function(e) {
 			return cache.addAll([
 				'/',
 				'./index.html',
-				'./static/',
+				'./static/css/pure-min.css',
+				'./static/css/icons.css',
+				'./static/css/grids-responsive-min.css',
+				'./static/css/fonts.css',
+				'./static/css/application.css',
+				'./static/fonts/icons.woff',
+				'./static/fonts/icons.ttf',
+				'./static/fonts/icons.svg',
+				'./static/fonts/icons.eot',
+				'./static/fonts/BrionRegular.ttf',
+				'./static/fonts/BrionLight.ttf',
+				'./static/fonts/BrionBold.ttf',
+				'./static/img/backgrounds/bg_cartographer.png',
 				'./resume.pdf'
 				]);
 		})
